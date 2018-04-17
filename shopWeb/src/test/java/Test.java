6fc477e612e659a188c73843815ba7ac4ef9e436
季常 <jichang@juanpi.com>
2018-04-18 00:48:43 +0800

    添加log4j

--- shopWeb/src/test/java/Test.java
+++ shopWeb/src/test/java/Test.java
@@ -1,5 +1,9 @@
+import com.endse.HelloWorld;
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 
 /**
  * Created by Administrator on 2018/4/17.
@@ -8,7 +12,9 @@ public class Test {
     @org.junit.Test
     public void test(){
         ApplicationContext ac = new ClassPathXmlApplicationContext("spring-context.xml");
-//        HelloWorld helloWorld = (HelloWorld)ac.getBean("helloWorld");
-//        helloWorld.say();
+        HelloWorld helloWorld = (HelloWorld)ac.getBean("helloWorld");
+        helloWorld.say();
+        Logger.getLogger(helloWorld.getClass()).error("ri");
+
     }
 }
